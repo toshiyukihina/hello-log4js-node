b36f95d134e627a83533be2e9ba624c01aa3cc8b
Toshiyuki Hina <toshiyuki.hina@gmail.com>
2015-01-02 01:27:25 +0900

    File appender

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 var log4js = require('log4js');
-var logger = log4js.getLogger();
+//log4js.clearAppenders();
+log4js.loadAppender('file');
+log4js.addAppender(log4js.appenders.file('./debug.log'), 'hello-log4js');
 
+var logger = log4js.getLogger('hello-log4js');
+logger.setLevel('WARN');
+
+logger.trace('Hello log4js');
 logger.debug('Hello log4js');
+logger.info('Hello log4js');
+logger.warn('Hello log4js');
+logger.error('Hello log4js');
+logger.fatal('Hello log4js');
